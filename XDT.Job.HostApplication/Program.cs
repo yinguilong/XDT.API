8b2b6d4d0b45f7b79821ce87053899222113f3c7
yinguilong <yinguilong@hotmail.com>
2018-04-04 16:20:05 +0800

    增加采集

---
+++ XDT.Job.HostApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace XDT.Job.HostApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var properties = new NameValueCollection
+            {
+                ["quartz.scheduler.instanceName"] = "XmlConfiguredInstance",
+                ["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz",
+                ["quartz.threadPool.threadCount"] = "5",
+                ["quartz.plugin.xml.type"] = "Quartz.Plugin.Xml.XMLSchedulingDataProcessorPlugin, Quartz.Plugins",
+                ["quartz.plugin.xml.fileNames"] = "~/quartz_jobs.xml",
+                // this is the default
+                ["quartz.plugin.xml.FailOnFileNotFound"] = "true",
+                // this is not the default
+                ["quartz.plugin.xml.failOnSchedulingError"] = "true"
+            };
+            ISchedulerFactory sf = new StdSchedulerFactory(properties);
+            IScheduler sched = sf.GetScheduler().Result;
+            sched.Start().Wait();
+            Console.Read();
+        }
+        public static async Task Run()
+        {
+            Console.WriteLine(AppContext.BaseDirectory);
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            IScheduler sched = await sf.GetScheduler();
+            await sched.Start();
+        }
+    }
+}
---
+++ XDT.Spider.Jobs/Alibaba/FetchTMJob.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XDT.Spider.Jobs.Alibaba
+{
+    public class FetchTMJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Hello kiny");
+            //同步方式实现该方法
+            return Task.FromResult(true);
+        }
+    }
+}
